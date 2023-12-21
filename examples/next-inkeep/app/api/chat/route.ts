42fc7fc6a61e8acb2116bbdace5ce337785ef949
Nick Gomez <nick@inkeep.com>
2023-12-20 20:18:18 -0500

    adding stream param to inkeep API

--- examples/next-inkeep/app/api/chat/route.ts
+++ examples/next-inkeep/app/api/chat/route.ts
@@ -42,6 +42,7 @@ export async function POST(req: Request) {
         chat_session: {
           messages: chatRequestBody.messages,
         },
+        stream: true,
       },
       client,
     });
